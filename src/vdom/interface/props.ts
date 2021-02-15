421077839fb1a1c1ec72cf158f3333603969cdb0
Aron Homberg <a.homberg@climatepartner.com>
2021-02-15 20:45:36 +0100

    feat: allow for forwardRef

--- src/vdom/interface/props.ts
+++ src/vdom/interface/props.ts
@@ -1,5 +1,9 @@
 import { IVirtualNode } from './ivirtual-node';
+import { Ref } from './ref';
 
 export interface Props {
   children?: Array<IVirtualNode>;
+
+  // allow for forwardRef cases
+  ref?: Ref;
 }
